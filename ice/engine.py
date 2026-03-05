835aeeac36f4b0eef9750196b94bab6dd1e05dba
Zephyr8760 <vnogueiralefort@gmail.com>
2026-03-05 16:41:31 +0100

    Update engine.py

--- ice/engine.py
+++ ice/engine.py
@@ -1,1 +1,14 @@
 # ICE engine
+def collect_ports_by_ip(events):
+
+    ports_by_ip = {}
+
+    for event in events:
+        timestamp, ip, port, status = event
+
+        if ip not in ports_by_ip:
+            ports_by_ip[ip] = set()
+
+        ports_by_ip[ip].add(port)
+
+    return ports_by_ip
